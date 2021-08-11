5348232e7febbbd9fb7462366d6a60690d2417f8
Jack H <8584913+jckhxn@users.noreply.github.com>
2021-08-11 19:25:37 -0400

    added vue example

---
+++ vue/basic-todo/src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="todo in todos" v-bind:key="todo">{{todo}}</li>
+    </ul>
+    <form v-on:submit.prevent="addTodo">
+    <input v-model="todoText" placeholder="What would you like todo?">
+    <button type="submit">Add Todo!</button>
+    </form>
+  </div>
+</template>
+<script>
+export default
+{
+  data:function() {
+    return {
+      // Define state variables.
+      todos:[],
+      todoText:'',
+
+  }
+  },
+  // Define your methods.
+  methods:
+  {
+   addTodo: function() {
+     this.todos = [...this.todos,this.todoText];
+     localStorage.setItem('todos', JSON.stringify(this.todos));
+
+     } 
+
+  },
+  mounted: function() {
+    const existingTodos = localStorage.getItem('todos');
+    this.todos = JSON.parse(existingTodos) || [];
+
+    }
+  }
+</script>
+<style scoped>
+
+</style>
